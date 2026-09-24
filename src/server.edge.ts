0b012e21c95dac2ef5fe5618275da6beb675f8dd
Angel Barrios <ae.barrios@hotmail.com>
2026-09-24 08:15:42 -0400

    feat: add Angular SSR support and configure Wrangler
    - Added a new server edge file for handling Angular SSR requests.
    - Updated package.json to include Wrangler for deployment.
    - Created wrangler.jsonc for project configuration.

---
+++ src/server.edge.ts
@@ -0,0 +1,10 @@
+import { AngularAppEngine, createRequestHandler } from "@angular/ssr";
+
+const angularApp = new AngularAppEngine();
+
+export const reqHandler = createRequestHandler(async (req) => {
+  const res = await angularApp.handle(req);
+  return res ?? new Response("Page not found", { status: 404 });
+});;
+
+export default {fetch: reqHandler};
